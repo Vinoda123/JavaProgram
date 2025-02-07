2a276045d8e03aa02dc1dac1ad418ab2e50cf810
Vinoda123 <14vinodakn@gmail.com>
2025-02-07 13:25:06 +0530

    CheckIfArrayIsSorted

---
+++ src/Programs/CheckIfArrayIsSorted.java
@@ -0,0 +1,19 @@
+package Programs;
+
+public class CheckIfArrayIsSorted {
+
+	public static void main(String[] args) {
+		
+		int [] arr = {23, 22, 25, 28, 21};
+		boolean isSort = true;
+		
+		for(int i = 0; i < arr.length-1; i++) {
+			if(arr[i] > arr[i+1]) {
+				isSort = false;
+			}
+		}
+		System.out.println(isSort);
+
+	}
+
+}
