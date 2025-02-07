8e7a04890bc605c61c5d21dcf4f511ae7a4d8ce4
Vinoda123 <14vinodakn@gmail.com>
2025-02-07 12:42:28 +0530

    Count the Occurance of the Character

--- src/Programs/CountTheoccuranceOfChar.java
+++ src/Programs/CountTheoccuranceOfChar.java
@@ -1,11 +1,21 @@
 package Programs;
 
+import java.util.HashMap;
+
 public class CountTheoccuranceOfChar {
 
 	public static void main(String[] args) {
 		
+		String word = "UshaVinoda";
+		char [] charecter = word.toCharArray();
 		
-
+		HashMap<Character, Integer> map = new HashMap<Character, Integer>();
+		
+		for(int i = 0; i <charecter.length; i++) {
+			char c = charecter[i];
+			map.put(c, map.getOrDefault(c, 0)+1);
+		}
+		System.out.println(map);
 	}
 
 }
