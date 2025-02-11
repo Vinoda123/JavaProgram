54af05dd6085791abc1efbd22d037f949f2025fa
Vinoda123 <14vinodakn@gmail.com>
2025-02-11 12:02:28 +0530

    StringAnagram

---
+++ src/Programs/StringAnagram.java
@@ -0,0 +1,25 @@
+package Programs;
+
+import java.util.Arrays;
+
+public class StringAnagram {
+
+	public static void main(String[] args) {
+		
+		String S1 = "night";
+		String S2 = "thingw";
+		
+		char [] arr1 = S1.toCharArray();
+		char [] arr2 = S2.toCharArray();
+		
+		Arrays.sort(arr1);
+		Arrays.sort(arr2);
+
+		if(Arrays.equals(arr1, arr2)) {
+			System.out.println("is amagram");
+		} else {
+			System.out.println("is not anagram");
+		}
+	}
+
+}
