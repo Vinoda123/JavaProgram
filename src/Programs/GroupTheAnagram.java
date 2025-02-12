a3955199dd625351d06d6fe32330ca24700ed9e2
Vinoda123 <14vinodakn@gmail.com>
2025-02-12 14:02:00 +0530

    GroupTheAnagram

---
+++ src/Programs/GroupTheAnagram.java
@@ -0,0 +1,32 @@
+package Programs;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupTheAnagram {
+
+	public static void main(String[] args) {
+		
+		String[] words = {"eat", "tea", "tan", "ate", "nat", "bat"};
+		
+		HashMap<String, List<String>> map  = new HashMap<String, List<String>>();
+		
+		for(int i = 0; i < words.length; i++) {
+			String word = words[i];
+			char [] charecter = word.toCharArray();
+			Arrays.sort(charecter);
+			
+			String newWord = new String(charecter);
+			
+			map.putIfAbsent(newWord, new ArrayList<>());
+			map.get(newWord).add(word);
+		}
+		
+		ArrayList<List< String>> result = new ArrayList<List <String>>(map.values());
+		System.out.println(result);
+
+	}
+
+}
